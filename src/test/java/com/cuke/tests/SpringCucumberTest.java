f64677a2040ba5478698e57486e7518ab16c160d
Gaurav Gaurav1 <gaurav.gaurav1@concentrix.com>
2017-10-03 17:11:44 +0530

    initial commit

---
+++ src/test/java/com/cuke/tests/SpringCucumberTest.java
@@ -0,0 +1,12 @@
+package com.cuke.tests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.junit.Cucumber;
+import cucumber.junit.Feature;
+
+@RunWith(Cucumber.class)
+@Feature("ATM.feature")
+public class SpringCucumberTest {
+
+}
